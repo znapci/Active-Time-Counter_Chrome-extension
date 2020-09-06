cc4f09475fe984bd01337c70adcd7c9025af4ee1
znapci <70022716+znapci@users.noreply.github.com>
2020-09-06 11:45:27 +0530

    Add counter

--- content.js
+++ content.js
@@ -1,11 +1,25 @@
+var secs = 0;
+var interval;
+function timerHandler() {
+    secs++;
+   }
+function startCounter() {
+    interval = window.setInterval(timerHandler, 1000);
+}
+function stopCounter() {
+    window.clearInterval(interval);
+   }
+
 window.onblur = function() {
     chrome.runtime.sendMessage({event: "blurred"}, function(response) {
-        console.log(response.msg)
+        console.log(response.msg);
+        startCounter();
       });
 }
 window.onfocus = function() {
     chrome.runtime.sendMessage({event: "focused"}, function(response){
-        console.log(response.msg)
+        console.log(response.msg);
+        stopCounter;
     });
 }
 /*
